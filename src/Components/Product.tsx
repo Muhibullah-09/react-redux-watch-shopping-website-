f3f1137994816262473f5ad30cf60d73c473beb0
Muhibullah-09 <mc2840696@gmail.com>
2020-09-13 09:59:32 +0500

    Update Product.tsx

--- src/Components/Product.tsx
+++ src/Components/Product.tsx
@@ -7,7 +7,6 @@ function Product() {
   const products = useSelector((state: productType[]) => state)
   const dispatch = useDispatch()
   const product = products.map((prod, ind) => {
-    console.log(ind)
     return (
       <div className="card col-lg-4  my-3  col-sm-12 col-md-6" key={ind}>
         <img className="card-img-top" width="100%" height="400px" src={prod.img} alt={ind.toString()} />
